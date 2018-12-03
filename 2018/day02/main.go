f1393f646ac8ec564796db4ba81b69186e00bfa8
Andrew Sellers <andrew@andrewcsellers.com>
2018-12-03 08:00:26 -0600

    Add in the 2018 codes

---
+++ 2018/day02/main.go
@@ -0,0 +1,328 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("12 == ", Do(demo))
+	fmt.Println("X == ", Do(input))
+	fmt.Println("fgij == ", Do2(demo2))
+	fmt.Println("??? == ", Do2(input))
+}
+
+func Do(input string) int {
+	twos, threes := 0, 0
+	for _, box := range strings.Split(input, "\n") {
+		if len(box) == 0 {
+			continue
+		}
+		seen := make(map[rune]int)
+		for _, r := range []rune(box) {
+			seen[r]++
+		}
+		for _, c := range seen {
+			if c == 2 {
+				twos++
+				break
+			}
+		}
+		for _, c := range seen {
+			if c == 3 {
+				threes++
+				break
+			}
+		}
+	}
+	return twos * threes
+}
+
+func Do2(input string) string {
+	subs := make(map[string]string)
+	for _, box := range strings.Split(input, "\n") {
+		for i := range box {
+			mod := ""
+			switch i {
+			case 0:
+				mod = box[1:]
+			case len(box) - 1:
+				mod = box[:len(box)-1]
+			default:
+				mod = box[:i] + box[i+1:]
+			}
+			if subs[mod] != "" && box != subs[mod] {
+				fmt.Println(mod, box, subs[mod])
+				return mod
+			}
+			subs[mod] = box
+		}
+	}
+	return ""
+}
+
+var demo = `abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab`
+var demo2 = `abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`
+
+var input = `rmyxgdlihczskunpfwbgqoeybv
+rmyxgdlksczskunpfwbjqkeatv
+rmybgdxibczskunpfwbjqoeatv
+rmyxgdlirczskuopfwbjqzeatv
+rmyxedlrhczskunpfwbyqoeatv
+rmyxfdlicczskunpfwbxqoeatv
+rmyxgvlihkzskunpfwbsqoeatv
+rmyxgdaihczvkunpfwblqoeatv
+nmyxgolihczskunpfwbjqieatv
+rhyxgdcihczskunifwbjqoeatv
+rmfxgdlihczskunpfwbvqgeatv
+smyxgdlihczskunsiwbjqoeatv
+rmyxgdcihcxskunpfwbrqoeatv
+rmyxgdlihczckuiqfwbjqoeatv
+rmyxxdwihczskunifwbjqoeatv
+rkzxgdlihczskunpfwhjqoeatv
+rmypgdlihczskunpfwbrqoeafv
+rmyxgplihczvkunpkwbjqoeatv
+rqyxgdlihdzskjnpfwbjqoeatv
+rmyxgdlihczskqnpswbjqoeaov
+mcyxgdlihczmkunpfwbjqoeatv
+rmyxgdlohczspunpowbjqoeatv
+tmyxgdlihczskunpfwbeqoeltv
+rmyxgdlibccskunpfwbjqoegtv
+rmyxgdlehczsaunpfwboqoeatv
+rmaxgdlihczseunpfwbjqojatv
+rmyxgdlijczskynpfwbjboeatv
+kmlxgdlilczskunpfwbjqoeatv
+rmsxgdlshczskenpfwbjqoeatv
+rmbxgdlihcmskgnpfwbjqoeatv
+rayxgdlihczskunpfwbjqoeaef
+umyxgdlisczskunpfdbjqoeatv
+rmyxgdlihczskunsfwbjqieatg
+rmbxgdlihczhkunpfwbjqoeamv
+rmyxgdlihczskeypfwbjqxeatv
+rmyxgkrihczskunptwbjqoeatv
+rmyxgdlihczskunpawbjqoexiv
+rmyxgdlihcrskqnpfwbjqceatv
+rmyxgblihczskjnpfwbjqieatv
+rmyggdlidczskunofwbjqoeatv
+rmyxgdlghczskunphwbjqomatv
+rmqxgdbihczskunpfnbjqoeatv
+rvyxgdlihczsgunpfwbjqoeanv
+royxgdlnhczskqnpfwbjqoeatv
+rmyxgdlihczskugpfwbkqreatv
+rmyxfdlihczskunppwejqoeatv
+rqyxgdlipczskunpfwbjqoeqtv
+rmyxgdlicczskunpnwbjqotatv
+rmyxodlihczskxnpfwijqoeatv
+rmyxrdyihczskunpftbjqoeatv
+rmtxgdyihwzskunpfwbjqoeatv
+tmyxcdliiczskunpfwbjqoeatv
+rmyxgdlihczskmnpfwbjjoeadv
+rmyxgdnihczskunpqwbjqojatv
+bmyxgdlihczskcnpfwboqoeatv
+rmysgdlihcyskudpfwbjqoeatv
+rmyxgdtihczsmuupfwbjqoeatv
+rmyxgdlihczssunpffbjqolatv
+rmyogdlihczsklnpfwbjqoxatv
+rmyxgjlihczskunpfwsjqoyatv
+rmyxgalshczskunpfwbuqoeatv
+rmyfgdlihczskunqfwbiqoeatv
+tmyxgdlihczskunotwbjqoeatv
+rmyxpdzihczskuopfwbjqoeatv
+rmyfgdlihczskunpfrbgqoeatv
+rmyxgdlwhczskhnofwbjqoeatv
+rmyxgdlihczsmudpfrbjqoeatv
+rmyxgdlihczokanpfwbjqooatv
+rmyxrdlihczskunppwjjqoeatv
+rmyxgdjihczskwnpowbjqoeatv
+mmyxgdlihczikunpfwbjqoeamv
+rmyxgflihczshunpwwbjqoeatv
+rmytghlihczskunpfwbjqoeatk
+rmyxgdlipczmbunpfwbjqoeatv
+rmyxgdlihczkkonpfwbjqomatv
+rmfxgslihczskunpfwujqoeatv
+dmyxgdlihczykunqfwbjqoeatv
+rmyxgalihcbskunpgwbjqoeatv
+rmyxgdlinczqkunpfwbjqopatv
+rmyxgdlihwzslunplwbjqoeatv
+rmypgdlihczskdtpfwbjqoeatv
+rmsxgdxieczskunpfwbjqoeatv
+rmyxgdlihczskwnpfxrjqoeatv
+rmyxgdlihzzskunpflbjpoeatv
+rslxgdlihczsnunpfwbjqoeatv
+rmyxgdlmcczskunpfwbjqoealv
+fmkxgdbihczskunpfwbjqoeatv
+rmyxgdiigczxkunpfwbjqoeatv
+rjyxgnlqhczskunpfwbjqoeatv
+ymyxgolihczskunpfmbjqoeatv
+hmyxgdlihczskuncfwbjqoejtv
+rmyxgqlihczzkunpfwbjqojatv
+rmgfgdlihczskunpfwbjgoeatv
+rmyxgdlfhczskunpfwbjqweaxv
+rmoxtdlihczskunpfwdjqoeatv
+ruyxgdlihczskunpfmbjnoeatv
+rmnxgflehczskunpfwbjqoeatv
+rmyugdlihczskunpfwfjroeatv
+rmyxddbihczskunpfwbjqoeutv
+rmyxgdlipczskunofbbjqoeatv
+gmyxgdlihczskunpfkbjroeatv
+rmyxgdllhcpskunpfwbjqqeatv
+rmyxgdlihchskunpfwbjqoelcv
+mmyxldlihczskuncfwbjqoeatv
+ryyxgdlxhczskcnpfwbjqoeatv
+rmyxpdlihczskyntfwbjqoeatv
+rmhxgdlibczskwnpfwbjqoeatv
+rmyxgdlihczskunpfwojbkeatv
+qmyxgdlihczskunpfwbjqoyatm
+rmyxgdlzhczskunpfwbjqoealr
+rmyegdliqczskunpfgbjqoeatv
+umyxgdlihczsvunpfwbfqoeatv
+rmyxgdoihfzskunpfmbjqoeatv
+rmyxgdlihcdskanpmwbjqoeatv
+rmyxgdyihczskunpfrbjqoeaov
+rcyxgdlihczskuegfwbjqoeatv
+rmyxgdlihgwskunpfwbjkoeatv
+rpyxgdlihmzskunpfwbjqoeatp
+rmyxgdlihhzskunpfwbjaoeapv
+rmyxgdsrhczskunpflbjqoeatv
+rmrxgdlihczskunpvwbjqoeabv
+rmcxgylihczskunpfwbjyoeatv
+rmkxgdlyhczsounpfwbjqoeatv
+rmyxgdqihczskunmfwbjqoratv
+rmyxgdlihczskunpfibjqofath
+rmyxgdliqczskunpqwbjqoeaev
+rmhxgdlizcjskunpfwbjqoeatv
+rmyxgdlfhcwskunpfwbjqoeaqv
+rmyxgdlchclskunpfwbdqoeatv
+rmyxgdluhczswunpfwbjqoeatt
+rmyxgdlzqczskunpfwbjqoeatq
+rmdxgdlihszskunpfwbwqoeatv
+rmyxgdlihszsvunpfwbjqueatv
+rmyxgdlhhczskunpffbjaoeatv
+rmrxgdlphczskunpfwbjqreatv
+hmyngdxihczskunpfwbjqoeatv
+rmyxgdlizczpkunpfwbyqoeatv
+rmyxbdlihyzskunlfwbjqoeatv
+rmyxgdlipczsqunnfwbjqoeatv
+rmyxgdlihcsskunpfxbjqoaatv
+rmyxgdljhcznkunpfwbjqfeatv
+rmaxgdlihczspunpfwbjqoqatv
+rsyxgdlihczskunpfwbjqoehcv
+rmyxgjlicczskunpfwbjqoeitv
+rwymgvlihczskunpfwbjqoeatv
+rmyxgdlipfzskunpfwbjqweatv
+rmyxgglihczskunpgwbjqoealv
+royxgdlihczskhnpfwbyqoeatv
+rmyxgdlihczskvnpfabkqoeatv
+rmyxgdlihczskunpfwhjwzeatv
+jlyxgdlihczskunpfwbjqzeatv
+rmyxgdlihccskunpfwwjqopatv
+rmyxgxlihczskuupfwbjqoeahv
+rmyxgdcihcbskungfwbjqoeatv
+tmyxgdlihczskunpfwbjmoeftv
+rkyxgdlioczskmnpfwbjqoeatv
+rmyxgdlrhczskulpfwbjaoeatv
+rmysgdlihczikunphwbjqoeatv
+rmyxgdlihczskuvpfwbjqoeyty
+fmyxgdlihczscunpfqbjqoeatv
+rfyxgdlihzzrkunpfwbjqoeatv
+rmyxgdlikczskunpfwbjqolath
+rmyxqdlihjzskunpfwbjqoeamv
+rmuxodiihczskunpfwbjqoeatv
+rmyygdliucuskunpfwbjqoeatv
+rmyxgdliwczskuppawbjqoeatv
+rmyxgdlihczskunprwbjqgehtv
+imyvgdlihczskunpfwbjqouatv
+rgyxgdluhczskunpflbjqoeatv
+rmgxgdlihczsdunpfwwjqoeatv
+gdyxgdlihczskunpfwbjqoeavv
+rmyxgdlihczskunpfwljjoektv
+rmexgdlihczskunpfwxjqoeytv
+rmyxqdlihcyskuwpfwbjqoeatv
+rmyxgdlihczskunpfiyjqcebtv
+amyngdlihczskunpfwbjqseatv
+rmzxgdlihczykubpfwbjqoeatv
+rmyxgdlihczhkuopfwbjsoeatv
+rmyxgdlihczskunpfwbaqowztv
+rmgxgdlihczslunpfwbjeoeatv
+rmytgdlzhczskunrfwbjqoeatv
+rmyxgdtihczskunafobjqoeatv
+rmyxgdlihczskuflfbbjqoeatv
+rmdxgdlihczskunpfwbjqoealj
+rbyxgdlihczskuppdwbjqoeatv
+rmyxhdiihcwskunpfwbjqoeatv
+rmmggdlfhczskunpfwbjqoeatv
+rmbxgblihczskuypfwbjqoeatv
+rmyxgslihczsjunpjwbjqoeatv
+rmyxgdlohczsaunpfwbjboeatv
+rmaxgdhihczskunpfwbjooeatv
+rmyxidlihczskunpfgbuqoeatv
+rmyxgdlihfzckznpfwbjqoeatv
+rmaqgdpihczskunpfwbjqoeatv
+rmyvgdlirczskunpfobjqoeatv
+rmdxgdlihczlkunpxwbjqoeatv
+rmyxgdlihczseunpfwbjvdeatv
+rmyxgdlihczskuhpfwbjqneath
+rmyxrdlihciskunpfwbjqoratv
+rmyxgdmihczsqunpftbjqoeatv
+rmyxgdlbhczskulpfbbjqoeatv
+rmoxgdlihczskunpfwbjqoeesv
+rmyxgdlihczskuijfwejqoeatv
+rmyxgdlihczskunpfwnkqoxatv
+rmyxgdvihmzskuupfwbjqoeatv
+rkyxedlihczskunpfcbjqoeatv
+rmyxgdjihczskunprwbjqieatv
+omyxgqgihczskunpfwbjqoeatv
+rmyxydlihczskunpfwkjqoentv
+rmbxgdlicczskunpfwbjqteatv
+emyxgdlihczskugpfwbjqneatv
+dmyxgflihczskunpfwbjqjeatv
+umyxgdlihczskunpfwbjloextv
+rmyxgdlihczsbunpfwbyqpeatv
+rmyxgdrihczsvunpcwbjqoeatv
+qmyxgdlihcwsknnpfwbjqoeatv
+ymyxgdlihczskunpfsbjqowatv
+rmyxgdlbhczskunpnvbjqoeatv
+rmyxfdlixczskunpfwbjqoertv
+rmyygdlihszrkunpfwbjqoeatv
+rmyxgxlihcpskunpfwbjqoeanv
+rmyxgdlihczskjnpfwbjqoprtv
+rmyxgdlisczfkunpfwbjqoeath
+rmyxgdlihczskunpfkbjqoeaji
+rmyxgylihczskunpfwbfqoeatl
+rmsxgdbihczskunpfwtjqoeatv
+smyxgdlihczskunpfwbjqcwatv
+rmyxgdlihczskunppjljqoeatv
+rmyxgdlihczskulpfdbjooeatv
+rmyxgdlihczskunpfibjqcebtv
+rmyxadlihczskunpgwbjyoeatv
+rmyxgdlihczdkunpvwbjqoeytv
+rmyxgdlihcvskunpfwbjxohatv
+rmyxgplihczskunpfgbjqoeauv
+rmyxgdlihcysrunmfwbjqoeatv
+rmyygdlihczskunpfwbjqvewtv
+rmyxgdlihczsmunpfwdjnoeatv
+rmyxgdbibczskunpfwbjuoeatv
+rmyfgdlihczskubpfwbjqoeatp
+rmyxgdlihczskuopfzijqoeatv
+rmyqgdlihczskunpwwbjqoeanv
+imyxgdlihczskunpfwbjqoqytv
+rmyxgdlixcoskbnpfwbjqoeatv
+rmyxgrlihccskunpfwbjqteatv
+rdyxgdlihcpskunpfwbjqoratv
+rmyxgdlihkzskunpfwbjmoeatj
+rmyxgslihczskcnpfjbjqoeatv
+rmyxgdlihczsqunqfwdjqoeatv
+rjyxgdlyhczbkunpfwbjqoeatv
+rmyxudlihczjkunpfwbjqzeatv`
